f5d59e6343d96b681a98eb4cae65591776290e0c
marcos-martins <m.martins@outlook.com>
2018-01-14 13:56:20 -0200

    Add login, me, delete token routers

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,9 +2,9 @@ const _ = require('lodash');
 const express = require('express');
 const {User} = require('./../models/user');
 const userRouter = express.Router();
+var {authenticate} = require('./../middleware/authenticate');
 
-
-userRouter.post('/me', function (req, res) {
+userRouter.post('', function (req, res) {
   var body = _.pick(req.body,['email','password','name','photo']);
   
   var user = new User(body);
@@ -17,6 +17,31 @@ userRouter.post('/me', function (req, res) {
     res.status(400).send(e);
   });
     
+});
+
+userRouter.get('/me',authenticate,(req, res) => {
+  res.send(req.user);
+});
+
+userRouter.post('/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+    //res.send(user);
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
+userRouter.delete('/me/token',authenticate,(req, res) => {
+  req.user.removeToken(req.token).then(()=>{
+    res.status(200).send();
+  }, () =>{
+    res.status(400).send();
+  });
 });
 
 module.exports = {userRouter};
